582fbe954fb4feb90139499044251bcbf8fbe4ec
function2 <luolx21@mails.tsinghua.edu.cn>
2022-06-04 20:09:58 +0800

    modify interface

--- monai/networks/nets/__init__.py
+++ monai/networks/nets/__init__.py
@@ -100,7 +100,7 @@ from .senet import (
     seresnext50,
     seresnext101,
 )
-from .swin_unetr import PatchMerging, PatchMergingV2, SwinUNETR
+from .swin_unetr import PatchMerging, PatchMergingV2, SwinUNETR, SwinTransformer, SwinUnetrDecoder
 from .torchvision_fc import TorchVisionFCModel
 from .transchex import BertAttention, BertMixedLayer, BertOutput, BertPreTrainedModel, MultiModal, Pooler, Transchex
 from .unet import UNet, Unet
