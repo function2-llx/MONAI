10746cc30453eb07879c72dfaa017aafc08129c3
function2 <luolx21@mails.tsinghua.edu.cn>
2022-08-14 04:57:59 +0800

    surface distance return nan when empty

--- monai/metrics/surface_distance.py
+++ monai/metrics/surface_distance.py
@@ -151,8 +151,12 @@ def compute_average_surface_distance(
         (edges_pred, edges_gt) = get_mask_edges(y_pred[b, c], y[b, c])
         if not np.any(edges_gt):
             warnings.warn(f"the ground truth of class {c} is all 0, this may result in nan/inf distance.")
+            asd[b, c] = np.nan
+            continue
         if not np.any(edges_pred):
             warnings.warn(f"the prediction of class {c} is all 0, this may result in nan/inf distance.")
+            asd[b, c] = np.nan
+            continue
         surface_distance = get_surface_distance(edges_pred, edges_gt, distance_metric=distance_metric)
         if symmetric:
             surface_distance_2 = get_surface_distance(edges_gt, edges_pred, distance_metric=distance_metric)
