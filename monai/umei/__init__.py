6b5c6df15468d324ee0fd984b8a8ec8219c9c1ff
function2 <luolx21@mails.tsinghua.edu.cn>
2023-03-27 21:56:08 +0800

    minor adjust umei api

--- monai/umei/__init__.py
+++ monai/umei/__init__.py
@@ -5,20 +5,21 @@ from dataclasses import dataclass, field
 import torch
 from torch import nn
 
-@dataclass
-class BackboneOutput:
-    cls_feature: torch.Tensor = field(default=None)
-    feature_maps: list[torch.Tensor] = field(default_factory=list)
-
 class Backbone(nn.Module):
     def forward(self, img: torch.Tensor, *args, **kwargs) -> BackboneOutput:
         raise NotImplementedError
 
 @dataclass
+class BackboneOutput:
+    cls_feature: torch.Tensor = field(default=None)
+    feature_maps: list[torch.Tensor] = field(default_factory=list)
+
+
+class Decoder(nn.Module):
+    def forward(self, backbone_feature_maps: list[torch.Tensor], x_in: torch.Tensor) -> DecoderOutput:
+        raise not NotImplementedError
+
+@dataclass
 class DecoderOutput:
     # low->high resolution
     feature_maps: list[torch.Tensor]
-
-class Decoder(nn.Module):
-    def forward(self, backbone_feature_maps: list[torch.Tensor], x_in) -> DecoderOutput:
-        raise not NotImplementedError
