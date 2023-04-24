b492b9fd4901e8fe8a0b656ae177ba0a6af52c94
function2 <luolx21@mails.tsinghua.edu.cn>
2023-04-24 19:10:34 +0800

    fix track lambda

--- monai/transforms/utility/array.py
+++ monai/transforms/utility/array.py
@@ -837,7 +837,7 @@ class Lambda(InvertibleTransform):
             raise TypeError(f"func must be None or callable but is {type(func).__name__}.")
         self.func = func
         self.inv_func = inv_func
-        # self.track_meta = track_meta
+        self.track_meta = track_meta
 
     def __call__(self, img: NdarrayOrTensor, func: Callable | None = None):
         """
